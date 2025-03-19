c79307369746345204d863a01de5f527461da6a8
jPuppo <liam.b.dodge@gmail.com>
2025-03-19 16:57:27 -0700

    first release version

--- script.js
+++ script.js
@@ -1,129 +1,41 @@
-function loadFiles() {
-	let fetchProgress = 0;
-	fetchFiles.forEach(fileStruct => {
-		fetch(fileStruct.file)
-			.then(response => {
-				if (!response.ok) {
-					fetchProgress++;
-					return;
-				}
-				response.json()
-					.then(data => {
-						fetchResults[fileStruct.name] = data;
-						fetchProgress++;
-					});
-			})	
-	})
-	return new Promise(resolve => {
-		const spinFunction = () => {
-			if (!(fetchProgress < fetchFiles.length)) {
-				resolve();
-			} else {
-				setTimeout(spinFunction, 100);
-			}
-		}
-		spinFunction();
-	});
-}
-
 function changeCharacter()
 {
-	let character = characterDropdown.value;
-	paletteCharacter = character;
-	paletteMode = 0;
-	flashIndex = 1;
+	palette.character = elements.character.value;
+	allPalettes = paletteFile[palette.character];
+	paletteColor.index = 0;
+	updatePalette(true);
+	changePaletteVariant(true);
+	elements.paletteName.selectedIndex = 0;
+	elements.paletteWhichColor.innerHTML = "";
+	let option;
+	colorNames[palette.character].forEach((value, index) => {
+		option = document.createElement("option");
+		option.value = index;
+		option.label = value;
+		elements.paletteWhichColor.options.add(option);
+	})
+	elements.paletteWhichColor.selectedIndex = paletteColor.index;
+	changePaletteWhichColor();
 }
 
-function swapPaletteMode()
+function changePaletteVariant(doUpdateColor)
 {
-	if (!fileLoaded) {
-		return;
+	palette.variant = elements.paletteVariant.checked * 1;
+	if (doUpdateColor) {
+		updateColor();
 	}
-	paletteMode = 1 - paletteMode;
-	flashIndex = 1;
 }
 
-function onSuccessfulLoad()
+function changeEditAll()
 {
-	fileLoaded = true;
-	requestRefresh();
+	editAll = elements.editAll.checked;
 }
 
-function newFile()
+function changePaletteWhichColor()
 {
-	if (fileLoaded) {
-		if (!confirm("Overwrite the current file?")) {
-			return;
-		}
-	}
-	resetPalettes();
-	onSuccessfulLoad();
-	requestRefresh();
-}
-
-function resetPalettes()
-{
-	palettes = fetchResults.defaultPalettes;
-}
-
-function rgbaToHex(r, g, b, a)
-{
-	return ((r << 16) + (g << 8) + b).toString(16).padStart(6, "0") + a.toString(16).padStart(2, "0");
-}
-
-function rgbaFillStyle(drawingContext, r, g, b, a)
-{
-	drawingContext.fillStyle = "rgb(" + r + " " + g + " " + b + ")";
-	drawingContext.globalAlpha = a / 255;
-}
-
-function loadJSON()
-{
-	if (input.files.length == 0) {
-		alert("Please select a file first.")
-		return;
-	}
-	if (fileLoaded) {
-		if (!confirm("Overwrite the current file?")) {
-			return;
-		}
-	}
-	const file = input.files[0];
-	const reader = new FileReader();
-	reader.onload = () => {
-		try {
-		palettes = JSON.parse(reader.result);
-		onSuccessfulLoad();
-		} catch (error) {
-		alert("There was an error loading the JSON file:\n" + error.name + ": " + error.message);
-		}
-	}
-	reader.readAsText(file);
-}
-
-function exportJSON()
-{
-	if (!fileLoaded) {
-		alert("Please load a file first.");
-		return;
-	}
-	const json = JSON.stringify(palettes, null, "	");
-	const blob = new Blob([json], {type: "application/json"});
-	const url = URL.createObjectURL(blob);
-	const link = document.createElement("a");
-	link.href = url;
-	link.download = "custom_palettes.json";
-	link.click();
-	URL.revokeObjectURL(url);
-}
-
-function chooseRandom(...items)
-{
-	if (items.length == 0) {
-		return undefined;
-	}
-	let index = Math.floor(Math.random() * items.length);
-	return items[index];
+	paletteColor.name = elements.paletteWhichColor.value;
+	paletteColor.index = elements.paletteWhichColor.selectedIndex;
+	updateColor();
 }
 
 function loadImages()
@@ -133,9 +45,8 @@ function loadImages()
 		let loadFunction = function() {
 			this.onload = null;
 			
-			if (this.name == "pattern") {
+			if (this.imageName == "pattern") {
 				pattern = context.createPattern(this, "repeat");
-				previewReady++;
 				return;
 			}
 			
@@ -144,7 +55,7 @@ function loadImages()
 			imageCanvas.height = this.height;
 			const imageContext = imageCanvas.getContext("2d");
 
-			var colors = (this.name == "buster") ? defaultColors.buster : defaultColors.bom;
+			var colors = (this.imageName == "buster") ? defaultColors.buster : defaultColors.bom;
 
 			const masks = [];
 			for (let i = 0; i < colors.length; i++) {
@@ -153,7 +64,6 @@ function loadImages()
 			const rest = imageContext.createImageData(imageCanvas.width, imageCanvas.height);
 
 			imageContext.drawImage(this, 0, 0);
-			imageContext.globalCompositeOperation = "source-over";
 			const imageData = imageContext.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
 			let data = imageData.data;
 
@@ -188,28 +98,249 @@ function loadImages()
 			let restImage = new Image();
 			restImage.src = imageCanvas.toDataURL();
 
-			images[this.name] = {
+			images[this.imageName] = {
 				masks: maskImages,
 				rest: restImage,
 			}
 
 			imageCanvas.remove();
-
-			previewReady++;
 		};
 		loadFunction.bind(image);
 		image.onload = loadFunction;
-		image.name = struct.name;
+		image.imageName = struct.name;
 		image.src = "Images/" + struct.path;
 	});
 }
-	
-function requestRefresh()
+
+function loadJSON()
 {
-	requestAnimationFrame(refresh);
+	if (elements.file.files.length == 0) {
+		alert("Please select a file first.")
+		return;
+	}
+	if (fileLoaded) {
+		if (!confirm("Overwrite the current file?")) {
+			return;
+		}
+	}
+	const file = elements.file.files[0];
+	const reader = new FileReader();
+	reader.onload = () => {
+		try {
+			paletteFile = JSON.parse(reader.result);
+		} catch (error) {
+			alert("There was an error loading the JSON file:\n" + error.name + ": " + error.message);
+			return;
+		}
+		elements.fileName.value = file.name;
+		onSuccessfulLoad();
+		changeCharacter();
+	}
+	reader.readAsText(file);
 }
 
-function drawImage(name, palette)
+function exportJSON()
+{
+	if (!fileLoaded) {
+		alert("Please load a file first.");
+		return;
+	}
+	const json = JSON.stringify(paletteFile);
+	const blob = new Blob([json], {type: "application/json"});
+	const url = URL.createObjectURL(blob);
+	const link = document.createElement("a");
+	link.href = url;
+	let fileName = elements.fileName.value;
+	if (fileName == "") {
+		fileName = "custom_palettes";
+	}
+	link.download = fileName;
+	link.click();
+	URL.revokeObjectURL(url);
+}
+
+function newFile()
+{
+	if (fileLoaded) {
+		if (!confirm("Overwrite the current file?")) {
+			return;
+		}
+	}
+	paletteFile = structuredClone(defaultPalettes);
+	onSuccessfulLoad();
+	changeCharacter();
+	elements.fileName.value = "";
+}
+
+function onSuccessfulLoad()
+{
+	if (!fileLoaded)
+		requestRefresh();
+	fileLoaded = true;
+	sections.edit.style.display = "block";
+}
+
+function changePaletteName()
+{
+	palette.name = elements.paletteName.value;
+	updatePalette();
+	updateColor();
+}
+
+function changeColor() {
+	let color = hexToRgb(elements.color.value);
+	let alpha = Math.round(parseInt(elements.alpha.value));
+	color.push(alpha);
+	for (let i = 0; i < palette.id.variants.length; i++) {
+		if (i == palette.variant || editAll) {
+			getCurrentPalette(i)[paletteColor.index] = color;
+		}
+	}
+}
+
+function updateColor() {
+	console.log(paletteColor);
+	let color = structuredClone(getCurrentPalette(palette.variant)[paletteColor.index]);
+	let alpha = color[3];
+	color.splice(3, 1);
+	let hex = rgbaToHex(...color);
+	hex = "#" + hex;
+	elements.color.value = hex;
+	elements.alpha.value = alpha.toString();
+}
+
+function updatePalette(startFromZero, initialValueType)
+{
+	if (!fileLoaded) {
+		return;
+	}
+	flashIndex = 1;
+	let paletteGroup = paletteFile[palette.character];
+	if (startFromZero) {
+		palette.index = 0;
+		palette.id = paletteGroup[0];
+		palette.name = palette.id.name;
+	} else {
+		if (initialValueType == "index") {
+			palette.id = paletteGroup[palette.index];
+			palette.name = palette.id.name;
+		} else {
+			palette.index = paletteGroup.findIndex(value => palette.name == value.name);
+			palette.id = paletteGroup[palette.index];
+		}
+	}
+
+	elements.paletteName.innerHTML = "";;
+	let option;
+	paletteGroup.forEach(value => {
+		option = document.createElement("option");
+		option.value = value.name;
+		option.label = value.name;
+		elements.paletteName.options.add(option);
+	})
+	elements.paletteName.selectedIndex = palette.index;
+}
+
+function deletePalette() {
+	if (allPalettes.length < 2) {
+		alert("Sorry partner, you can't delete the last one.")
+		return;
+	}
+	if (!confirm("Really delete this palette?")) {
+		return;
+	}
+	allPalettes.splice(palette.index, 1);
+	if (palette.index > 0) {
+		palette.index--;
+	}
+	updatePalette(false, "index");
+}
+
+function addPalette() {
+	let newName = prompt("Name the copied palette.", "")
+	if (newName == null) {
+		return;
+	}
+	let paletteNames = allPalettes.map(thisPalette => {
+		return thisPalette.name;
+	})
+	if (paletteNames.includes(newName)) {
+		alert("Sorry partner, that palette already exists.")
+		return;
+	}
+	pal = structuredClone(allPalettes[palette.index]);
+	pal.name = newName;
+	allPalettes.push(pal)
+	palette.index = allPalettes.length - 1;
+	updatePalette(false, "index");
+	elements.paletteName.selectedIndex = elements.paletteName.options.length - 1;
+	changePaletteName();
+}
+
+function rgbaToHex(r, g, b, a)
+{
+	let hex = ((r << 16) + (g << 8) + b).toString(16).padStart(6, "0");
+	if (a || a == 0) {
+		return hex + a.toString(16).padStart(2, "0");
+	} else {
+		return hex;
+	}
+}
+
+function hexToRgb(hex) {
+	hex = hex.replace("#", "");
+
+	const r = parseInt(hex.substring(0, 2), 16);
+	const g = parseInt(hex.substring(2, 4), 16);
+	const b = parseInt(hex.substring(4, 8), 16);
+
+	return [r, g, b];
+}
+
+function rgbaFillStyle(drawingContext, r, g, b, a)
+{
+	drawingContext.fillStyle = "rgb(" + r + " " + g + " " + b + ")";
+	drawingContext.globalAlpha = a / 255;
+}
+
+function chooseRandom(...items)
+{
+	if (items.length == 0) {
+		return undefined;
+	}
+	let index = Math.floor(Math.random() * items.length);
+	return items[index];
+}
+
+function loadFiles() {
+	let fetchProgress = 0;
+	fetchFiles.forEach(fileStruct => {
+		fetch(fileStruct.file)
+			.then(response => {
+				if (!response.ok) {
+					fetchProgress++;
+					return;
+				}
+				response.json()
+					.then(data => {
+						fetchResults[fileStruct.name] = data;
+						fetchProgress++;
+					});
+			})	
+	})
+	return new Promise(resolve => {
+		const spinFunction = () => {
+			if (!(fetchProgress < fetchFiles.length)) {
+				resolve();
+			} else {
+				setTimeout(spinFunction, 100);
+			}
+		}
+		spinFunction();
+	});
+}
+
+function drawImage(name, drawPalette)
 {
 	let tempCanvas = document.createElement("canvas");
 	images[name].masks.forEach(function(mask, index) {
@@ -218,12 +349,22 @@ function drawImage(name, palette)
 		let tempContext = tempCanvas.getContext("2d");
 		tempContext.drawImage(mask, 0, 0);
 		tempContext.globalCompositeOperation = "source-in";
-		rgbaFillStyle(tempContext, ...palette[index]);
+		rgbaFillStyle(tempContext, ...drawPalette[index]);
 		tempContext.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
 		context.drawImage(tempCanvas, 0, 0);
 	})
 	tempCanvas.remove();
 	context.drawImage(images[name].rest, 0, 0);
+}
+
+function getCurrentPalette(variant)
+{
+	return palette.id.variants[variant];
+}
+
+function requestRefresh()
+{
+	requestAnimationFrame(refresh);
 }
 
 function refresh(timestamp)
@@ -233,18 +374,13 @@ function refresh(timestamp)
 	}
 	lastTimestamp = timestamp;
 	
-	if (previewReady < imageStructs.length) {
-		requestRefresh();
-		return;
-	}
-	
 	context.fillStyle = "#bfbfbf";
+	context.globalAlpha = 1
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
+	pattern.setTransform(transformMatrix);
 	patternTransform = 20 * deltaTime;
 	transformMatrix = transformMatrix.translateSelf(patternTransform, patternTransform);
-	
-	pattern.setTransform(transformMatrix);
 
 	flashIndex += 20 * deltaTime;
 	flashIndex %= 2;
@@ -253,19 +389,31 @@ function refresh(timestamp)
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
 	if (fileLoaded) {
-		let paletteNum = 0;
-		if (paletteCharacter == "buster") {
-			drawImage("buster", palettes.buster[paletteNum + paletteMode].colors);
+		let variantZero = getCurrentPalette(0);
+		let variantOne = getCurrentPalette(1);
+		let variantCurrent = getCurrentPalette(palette.variant);
+		let canvasDivision = canvas.width / variantCurrent.length;
+		context.fillStyle = "#bfbfbf";
+		context.fillRect(0, 0, canvas.width, 30);
+		variantCurrent.forEach((color, index) => {
+			rgbaFillStyle(context, ...color);
+			context.fillRect(index * canvasDivision + 2, 2, canvasDivision - 4, 26);
+		})
+		context.globalAlpha = 1;
+		if (palette.character == "buster") {
+			if (variantCurrent) {
+				drawImage("buster", variantCurrent);
+			}
 		} else {
 			let bomImage = "bom";
-			if (paletteMode > 0) {
+			if (palette.variant == 1) {
 				bomImage = "bom edge";
 			}
-			drawImage(bomImage, palettes.bom[paletteNum + (paletteMode * Math.floor(flashIndex))].colors);
-			drawImage("pipe", palettes.bom[paletteNum].colors);
-		
+			if (variantZero && variantOne) {
+				drawImage(bomImage, ((palette.variant > 0 && (Math.floor(flashIndex) == 1)) ? variantOne : variantZero));
+				drawImage("pipe", variantZero);
+			}
 		}
-		
 	}
 	
 	requestRefresh();
@@ -279,92 +427,64 @@ const fetchFiles = [
 	{
 		"file": "defaultColors.json",
 		"name": "defaultColors",
+	},
+	{
+		"file": "images.json",
+		"name": "imageStructs",
+	},
+	{
+		"file": "colorNames.json",
+		"name": "colorNames",
 	}
 ]
 const fetchResults = {};
 
-const content = document.getElementById("content");
-content.style.display = "none";
+const sections = {
+	content: document.getElementById("sectionContent"),
+	edit: document.getElementById("sectionEdit"),
+}
+const elements = {
+	file: document.getElementById("inputFile"),
+	fileName: document.getElementById("inputFileName"),
+	character: document.getElementById("inputCharacter"),
+	paletteVariant: document.getElementById("inputPaletteVariant"),
+	paletteName: document.getElementById("inputPaletteName"),
+	paletteWhichColor: document.getElementById("inputPaletteWhichColor"),
+	color: document.getElementById("inputColor"),
+	alpha: document.getElementById("inputAlpha"),
+	editAll: document.getElementById("inputEditAll"),
+}
 
-const fileInput = document.getElementById("input");
-const characterDropdown = document.getElementById("characterDropdown");
+var paletteFile = null;
+var allPalettes = null;
+var palette = {};
+var paletteColor = {};
+var editAll = false;
+changeEditAll();
+
 const canvas = document.getElementById("preview");
 const context = canvas.getContext("2d");
-
-var palettes;
-changeCharacter();
+context.imageSmoothingEnabled = false;
 
 var fileLoaded = false;
-var defaultColors, defaultPalettes;
+var defaultColors, defaultPalettes, imageStructs, colorNames;
 var pattern = null;
 
-var previewReady = 0;
-
 const images = {};
-const imageStructs = [
-	{
-		"name": "buster",
-		"path": "buster.png",
-		"type": 0,
-	},
-	{
-		"name": "bom",
-		"path": "bom.png",
-		"type": 0,
-	},
-	{
-		"name": "bom edge",
-		"path": "bom edge.png",
-		"type": 0,
-	},
-	{
-		"name": "pattern",
-		"path": "pattern.png",
-		"type": 1,
-	},
-	{
-		"name": "pipe",
-		"path": "pipe.png",
-		"type": 0,
-	},
-];
-const colorNames = {
-	"buster": [
-		"White",
-		"Outline",
-		"Gloves",
-		"Pants",
-		"Skin",
-		"Nose",
-		"Nose Outline",
-		"Spark",
-		"Spark Outline",
-	],
-	"bom": [
-		"White",
-		"Outline",
-		"Spark Outline",
-		"Color Dark",
-		"Color",
-		"Color Light",
-		"Eyes",
-		"Top",
-	],
-}
 
 var lastTimestamp = null;
 var deltaTime = 0;
 var patternTransform, flashIndex;
 var transformMatrix = new DOMMatrix([1, 0, 0, 1, 0, 0]);
 
-resetPalettes();
 loadFiles()
 	.then(() => {
 		context.fillStyle = "#bfbfbf";
 		context.fillRect(0, 0, canvas.width, canvas.height);
 		defaultColors = fetchResults.defaultColors;
 		defaultPalettes = fetchResults.defaultPalettes;
+		imageStructs = fetchResults.imageStructs;
+		colorNames = fetchResults.colorNames;
 		loadImages();
-		content.style.display = "block";
+		sections.content.style.display = "block";
 	})
-
